6ff6083a1a80c47252bfef34ffb056316d0917f3
Renjie Liu <anlrj@qq.com>
2024-11-21 18:04:39 -0500

    .

---
+++ 3_ecc.sql
@@ -0,0 +1,28 @@
+drop table if exists #gf 
+
+; with cte as 
+(
+    select id = 0, val = 1 
+    union all 
+    select id + 1
+        , case when val*2 >=256 then (val * 2) ^ 285  -- 11d 
+                else val*2 
+            end  
+    from cte  
+    where  id < 255
+)
+select id, val into #gf from cte
+order by 1 desc
+option (maxrecursion 0 )
+
+
+drop table if exists #gf_inv
+
+select id = val, val = id into #gf_inv from #gf 
+
+update #gf_inv set id = 0 where val = 0; 
+
+
+
+
+
